836602586fb6b345b59827efdd177144dbb6099e
haisapan <haisapan@hotmail.com>
2015-06-13 11:06:13 +0800

    buid a code generator using Asp.net MVC razor
    this is the first build, only create the project

---
+++ HaisaCodeGenerator/CodeGeneratorHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HaisaCodeGenerator
+{
+    public class CodeGeneratorHelper
+    {
+        private void RunGenerate()
+        {
+            var template = "";
+
+            var templateFile =
+                @"D:\Users\haisa.pan\Documents\Visual Studio 2013\Projects\HaisaCodeGenerator\HaisaCodeGenerator\1.cshtml";
+            string content = System.IO.File.ReadAllText(templateFile);
+            var result = Engine.Razor.RunCompile(content, "new", null,
+                new
+                {
+                    Name = "haisa"
+                });
+            var test = "";
+        }
+    }
+}
